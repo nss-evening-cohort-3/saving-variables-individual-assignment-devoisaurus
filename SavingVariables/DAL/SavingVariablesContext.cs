8e4c02f2c5813fca9f1f5bdc1d37f559dd5651d4
Andi DeVois <adevois@gmail.com>
2016-09-23 15:26:56 -0700

    ERD, setting up DAL and Expression model with keys

---
+++ SavingVariables/DAL/SavingVariablesContext.cs
@@ -0,0 +1,14 @@
+﻿using SavingVariables.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingVariables.DAL
+{
+    public class SavingVariablesContext : DbContext
+    {
+        public DbSet<Expression> Expressions { get; set; }
+    }
+}
