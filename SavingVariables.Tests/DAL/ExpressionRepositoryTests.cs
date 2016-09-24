a9e90dff85499da6c17b9250c9766a6880cfca2c
Andi DeVois <adevois@gmail.com>
2016-09-23 23:56:57 -0700

    repository and testing set up

---
+++ SavingVariables.Tests/DAL/ExpressionRepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SavingVariables.DAL;
+using System.Collections.Generic;
+using SavingVariables.Models;
+
+namespace SavingVariables.Tests.DAL
+{
+    [TestClass]
+    public class ExpressionRepositoryTests
+    {
+        [TestMethod]
+        public void EnsureRepoCanCreateAnInstance()
+        {
+            ExpressionRepository repo = new ExpressionRepository();
+            Assert.IsNotNull(repo);
+        }
+    }
+}
